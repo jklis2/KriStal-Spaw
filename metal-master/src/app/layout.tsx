6f8b9b065a228465be5786c1ae90b2c8720b1533
Jakub Kliś <jakubklis2201@gmail.com>
2026-03-15 17:26:01 +0100

    feat: Add Google Analytics and Microsoft Clarity integration

--- metal-master/src/app/layout.tsx
+++ metal-master/src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
+//import Clarity from "@/components/Clarity";
+//import Analytics from "@/components/Analytics";
 
 const roboto = Roboto({
   weight: ["400", "700"],
@@ -33,10 +35,10 @@ export default function RootLayout({
         className={`${roboto.variable} ${oswald.variable} font-roboto antialiased bg-background text-foreground`}
       >
         <ThemeProvider>
+          {/* <Clarity /> */}
+          {/* <Analytics /> */}
           <Navbar />
-          <main className="min-h-screen">
-            {children}
-          </main>
+          <main className="min-h-screen">{children}</main>
           <Footer />
         </ThemeProvider>
       </body>
