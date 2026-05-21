de9ef445e0786be6802ef0e43f6af41cd1eee6f9
jklis2 <jakubklis2201@gmail.com>
2026-05-21 16:44:18 +0200

    feat: Add images to offer cards

--- metal-master/src/consts/offerItems.ts
+++ metal-master/src/consts/offerItems.ts
@@ -8,41 +8,41 @@ export const offers: OfferItem[] = [
  {
   title: "Bramy przesuwne",
   description: "Bramy przesuwne, zapewniające wygodę użytkowania, trwałość i płynne działanie.",
-  image: "/images/photoPlaceholder.webp",
+  image: "/offer/brama_przesuwna.webp",
 },
 {
   title: "Bramy dwuskrzydłowe",
   description: "Bramy dwuskrzydłowe, łączące funkcjonalność i niezawodność na każdy podjazd.",
-  image: "/images/photoPlaceholder.webp",
+  image: "/offer/brama_dwuskrzydlowa.webp",
 },
 {
   title: "Ogrodzenia systemowe",
   description: "Ogrodzenia systemowe, zapewniające estetykę, bezpieczeństwo i dopasowanie do każdej posesji.",
-  image: "/images/photoPlaceholder.webp",
+  image: "/offer/ogrodzenie_systemowe.webp",
 },
 {
   title: "Balustrady balkonowe",
   description: "Balustrady balkonowe z szerokim zakresem systemu montażowego.",
-  image: "/images/photoPlaceholder.webp",
+  image: "/offer/balustrada_balkonowa.webp",
 },
 {
   title: "Balustrady wewnętrzne",
   description: "Balustrady wewnętrzne dopasowane do charakteru wnętrza, tworzące spójną i estetyczną całość.",
-  image: "/images/photoPlaceholder.webp",
+  image: "/offer/balustrada_wewnetrzna.webp",
 },
 {
   title: "Balustrady szklane",
   description: "Nowoczesne balustrady szklane, łączące konstrukcję stalową ze szkłem VSG ESG.",
-  image: "/images/photoPlaceholder.webp",
+  image: "/offer/balustrada_szklana_2.webp",
 },
 {
   title: "Balustrady francuskie",
   description: "Balustrady francuskie o lekkiej i estetycznej formie, idealne do nowoczesnych i klasycznych elewacji.",
-  image: "/images/photoPlaceholder.webp",
+  image: "/offer/balustrada_francuska.webp",
 },
 {
   title: "Inne projekty",
   description: "Realizujemy indywidualne projekty stalowe, dopasowane do potrzeb i pomysłów naszych klientów.",
-  image: "/images/photoPlaceholder.webp",
+  image: "/offer/stol_inne_projekty.webp",
 }
 ];
